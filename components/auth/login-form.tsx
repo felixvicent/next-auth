8b171dc0952e8162c1288f6a84524373ffbcfb7f
Félix Vicente <felixvicent1306@gmail.com>
2024-01-02 16:35:01 -0300

    OAuth (Google & Github)

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,6 +5,7 @@ import * as z from 'zod';
 import { useState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useSearchParams } from 'next/navigation';
 
 import { LoginSchema } from '@/schemas';
 import { CardWrapper } from "@/components/auth/card-wrapper";
@@ -23,6 +24,11 @@ import { FormSuccess } from '@/components/form-success';
 import { login } from '@/actions/login';
 
 export function LoginForm() {
+  const searchParams = useSearchParams();
+  const urlError = searchParams.get("error") === 'OAuthAccountNotLinked'
+    ? "Email already in user with different provider!"
+    : '';
+
   const [isPending, startTransition] = useTransition();
 
   const [error, setError] = useState<string | undefined>('');
@@ -42,8 +48,7 @@ export function LoginForm() {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
       });
     })
   }
@@ -95,7 +100,7 @@ export function LoginForm() {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button type='submit' className='w-full' disabled={isPending}>
             Login
